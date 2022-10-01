4087fb7cbb35c38b3a0407763faf4c19740404aa
Yulong Liu <yulo.liu@mail.utoronto.ca>
2022-10-01 17:34:54 -0400

    modify Polynomial.java and Driver.java

--- Driver.java
+++ Driver.java
@@ -1,16 +1,25 @@
-public class Driver {
-    public static void main(String [] args) {
+import java.io.*;
+
+public class Driver{
+    public static void main(String[] args) throws IOException{
         Polynomial p = new Polynomial();
         System.out.println(p.evaluate(3));
-        double [] c1 = {6,0,0,5};
-        Polynomial p1 = new Polynomial(c1);
-        double [] c2 = {0,-2,0,0,-9};
-        Polynomial p2 = new Polynomial(c2);
+        Polynomial p1 = new Polynomial(new double[]{6, 0, 0, 5});
+        Polynomial p2 = new Polynomial(new double[]{0, -2, 0, 0, -9});
         Polynomial s = p1.add(p2);
         System.out.println("s(0.1) = " + s.evaluate(0.1));
-        if(s.hasRoot(1))
-            System.out.println("1 is a root of s");
-        else
-            System.out.println("1 is not a root of s");
+        System.out.println("1 is " + (s.hasRoot(1) ? "a" : "not a") + " root of s");
+
+        Polynomial q1 = new Polynomial(new double[]{6, -2, 5}, new int[]{0, 1, 3});
+        Polynomial q2 = new Polynomial(new double[]{5, -3, 7}, new int[]{0, 2, 8});
+        Polynomial qq2 = new Polynomial(new File("poly"));
+
+        System.out.println(q1.add(q2).add(q1).add(q2));
+        q1.add(qq2).add(q1).add(qq2).saveToFile("sum");
+        System.out.println(new Polynomial(new File("sum")));
+
+        System.out.println(q1.multiply(q2).multiply(q1).multiply(q2));
+        q1.multiply(qq2).multiply(q1).multiply(q2).saveToFile("prod");
+        System.out.println(new Polynomial(new File("prod")));
     }
 }
--- Polynomial.java
+++ Polynomial.java
@@ -1,41 +1,110 @@
-public class Polynomial {
-    double[] coefficients;
+import java.io.*;
+import java.util.Arrays;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
-    public Polynomial() {
+public class Polynomial{
+    public double[] coefficients;
+    public int[] exponents;
+    private final int length;
+
+    //#region ctors
+
+    public Polynomial(){
         this.coefficients = new double[]{0};
+        this.exponents = new int[]{0};
+        this.length = 1;
     }
 
-    public Polynomial(double[] coefficients) {
+    public Polynomial(double[] coefficients){
         this.coefficients = coefficients;
+        this.exponents = IntStream.range(0, coefficients.length).toArray();
+        this.length = coefficients.length;
     }
 
-    public Polynomial add(Polynomial rhs) {
-        double[] longer, shorter;
-        if (this.coefficients.length > rhs.coefficients.length) {
-            longer = this.coefficients;
-            shorter = rhs.coefficients;
-        } else {
-            shorter = this.coefficients;
-            longer = rhs.coefficients;
-        }
-        Polynomial poly = new Polynomial(longer.clone());
-        for (int i = 0; i < shorter.length; i++) {
-            poly.coefficients[i] += shorter[i];
-        }
-        return poly;
+    public Polynomial(double[] coefficients, int[] exponents){
+        this.coefficients = coefficients;
+        this.exponents = exponents;
+        this.length = Math.min(coefficients.length, exponents.length);
     }
 
-    public double evaluate(double x) {
-        double d = 1.0;
-        double sum = 0.0;
-        for (double coefficient : this.coefficients) {
-            sum += d * coefficient;
-            d *= x;
-        }
-        return sum;
+    public Polynomial(Stream<Map.Entry<Integer, Double>> stream){
+        Map<Integer, Double> map = stream.collect(Collectors.toMap(
+            Map.Entry::getKey, Map.Entry::getValue, Double::sum
+        ));
+        this.exponents = map.keySet().stream().mapToInt(Integer::intValue).toArray();
+        this.coefficients = map.values().stream().mapToDouble(Double::doubleValue).toArray();
+        this.length = map.size();
     }
 
-    public boolean hasRoot(double x) {
+    public Polynomial(File f) throws FileNotFoundException{
+        this(Arrays.stream(new Scanner(f).nextLine().replaceFirst("^(?!-)", "+")
+            .replaceAll("([+-][0-9]+)(?=[-+$])", "$1x0")
+            .replaceAll("x(?![0-9])", "x1")
+            .replaceAll("(?<=[-+])x", "1x").split("(?=[-+])")
+        ).map(s -> s.split("x")).map(
+            split -> Map.entry(Integer.parseInt(split[1]), Double.parseDouble(split[0]))
+        ));
+    }
+    //#endregion
+
+    private Map<Integer, Double> map(){
+        return IntStream.range(0, this.length).boxed().collect(
+            Collectors.groupingBy(
+                i -> this.exponents[i],
+                Collectors.reducing(0.0, i -> this.coefficients[i], Double::sum)
+            )
+        );
+    }
+
+    @Override
+    public String toString(){
+        return this.map().toString();
+    }
+
+    public Polynomial add(Polynomial rhs){
+        return new Polynomial(Stream.of(this.map(), rhs.map()).flatMap(
+            map -> map.entrySet().stream()
+        ));
+    }
+
+    public Polynomial multiply(Polynomial rhs){
+        Set<Map.Entry<Integer, Double>> entries = rhs.map().entrySet();
+        return new Polynomial(this.map().entrySet().stream().flatMap(
+            e1 -> entries.stream().map(e2 -> Map.entry(
+                e1.getKey() + e2.getKey(), e1.getValue() * e2.getValue()
+            ))
+        ));
+    }
+
+    public double evaluate(double x){
+        return IntStream.range(0, this.length).mapToDouble(
+            i -> this.coefficients[i] * Math.pow(x, this.exponents[i])
+        ).sum();
+    }
+
+    public boolean hasRoot(double x){
         return this.evaluate(x) == 0;
+    }
+
+    public void saveToFile(String file) throws FileNotFoundException{
+        try(PrintStream ps = new PrintStream(file)){
+            ps.print(this.map().entrySet().stream().map(
+                entry -> {
+                    Double c = entry.getValue();
+                    if(c == 0.0) return "";
+                    String coefficient = c % 1 == 0 ? Integer.toString(c.intValue()) : Double.toString(c);
+                    Integer exponent = entry.getKey();
+                    String prefix = c > 0 ? "+" : "";
+                    if(exponent == 0) return prefix + coefficient;
+                    if(exponent == 1) return prefix + coefficient + 'x';
+                    return prefix + coefficient + 'x' + exponent;
+                }
+            ).collect(Collectors.joining()).replaceFirst("^\\+", ""));
+        }
     }
 }
